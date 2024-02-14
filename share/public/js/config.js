4dc67a303719c19a2b6718ccb64aaa4683ed91de
Stephen Vaz <stephenvaz2003@gmail.com>
2024-02-14 05:49:04 +0530

    added turn configuration

---
+++ share/public/js/config.js
@@ -0,0 +1,23 @@
+
+const turnConfig = {
+  iceServers: [
+    { urls: [
+      "stun:bn-turn1.xirsys.com"
+    ] },
+    {
+      username: 
+      "67JVSBLop5DqymSoHACKdBZmSQz_19LJX8Hk-bkVSGNu_FhzuoxZ52EFYmEX43-0AAAAAGXLXc9zdGVwaGVuMDM=", 
+      credential: 
+      "d588ed62-ca69-11ee-a4f6-0242ac140004",
+      urls: [
+        "turn:bn-turn1.xirsys.com:80?transport=udp",
+        "turn:bn-turn1.xirsys.com:3478?transport=udp",
+        "turn:bn-turn1.xirsys.com:80?transport=tcp",
+        "turn:bn-turn1.xirsys.com:3478?transport=tcp",
+        "turns:bn-turn1.xirsys.com:443?transport=tcp",
+        "turns:bn-turn1.xirsys.com:5349?transport=tcp"
+      ],
+    },
+  ],
+
+};
