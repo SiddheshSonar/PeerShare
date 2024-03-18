893e34079760fd2e5486738facbafdcf7a981604
Stephen Vaz <stephenvaz2003@gmail.com>
2024-03-19 01:24:24 +0530

    ntp based clock syncing complete

---
+++ clock/client/client.js
@@ -0,0 +1,31 @@
+const NTP = require('ntp-time').Client;
+const client = new NTP('localhost', 1200, { timeout: 5000 });
+
+let time = new Date();
+
+async function sync() {
+	try {
+
+		const t = await client.syncTime();
+        console.log("Clock Synced")
+        time = t.time;
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+// sync clock after every 30s
+setInterval(sync, 30 * 1000);
+
+setInterval(() => {
+    console.log(time.toLocaleString(
+        'en-US',
+        {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: true
+        }
+    ));
+    time = new Date(time.getTime() + 1000);
+}, 1000);
